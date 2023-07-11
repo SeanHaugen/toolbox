75238fe85150d716b88810b4ba2142f7a6af9ded
Sean Haugen <shaugen@showdowndisplays.com>
2023-07-11 12:44:41 -0500

    added react

---
+++ toolbox/src/App.js
@@ -0,0 +1,11 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src="http://toolbox.signzoneinc.com/"></iframe>
+    </div>
+  );
+}
+
+export default App;
